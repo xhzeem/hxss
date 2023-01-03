dd47dbb94d4612e1598f62a344602bbbe5a898ab
أنس روبي <34074156+xhzeem@users.noreply.github.com>
2023-01-03 04:28:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,137 +2,203 @@ package main
 
 import (
 	"bufio"
-	"flag"
+	"crypto/tls"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"net/url"
 	"os"
-	"sort"
 	"strings"
+	"sync"
+	"time"
 )
 
+type paramCheck struct {
+	url   string
+	param string
+}
+
+var transport = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: time.Second,
+		DualStack: true,
+	}).DialContext,
+}
+
+var httpClient = &http.Client{
+	Transport: transport,
+}
+
 func main() {
-	
-	var newValue string
-	flag.StringVar(&newValue, "v", "", "Set the custom value to modify the URLs upon")
 
-	var addParam string
-	flag.StringVar(&addParam, "p", "", "Add a custom parameter to the URL")
+	httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
 
-	var replaceMode bool
-	flag.BoolVar(&replaceMode, "r", false, "Replace the value instead of appending it")
-
-	var singleMode bool
-	flag.BoolVar(&singleMode, "s", false, "Modify a single parameter at a time")
-
-	var decodeMode bool
-	flag.BoolVar(&decodeMode, "d", false, "URL decode the values of the paramters")
-
-	flag.Parse()
-
-	seen := make(map[string]bool)
-
-	// read URLs on stdin, then modify the values in the query string with some user-provided value
 	sc := bufio.NewScanner(os.Stdin)
-	for sc.Scan() {
-		u, err := url.Parse(sc.Text())
+
+	initialChecks := make(chan paramCheck, 40)
+
+	appendChecks := makePool(initialChecks, func(c paramCheck, output chan paramCheck) {
+		reflected, err := checkReflected(c.url)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to parse url %s [%s]\n", sc.Text(), err)
-			continue
+			return
 		}
 
-		// Add the parameter to the URL if the `p` flag is specified
-		if addParam != "" {
-			if u.RawQuery == "" {
-				// No parameters in the URL, so just add the new parameter
-				u.RawQuery = addParam + "=" + newValue
-			} else {
-				// There are already parameters in the URL, so check if the specified parameter exists
-				qs := u.Query()
-				if _, exists := qs[addParam]; !exists {
-					// The parameter doesn't exist, so add it
-					qs.Set(addParam, newValue)
-					if (decodeMode){
-						u.RawQuery, _ = url.QueryUnescape(qs.Encode())
-					} else {
-						u.RawQuery = qs.Encode()
-					}
-				}
-			}
+		if len(reflected) == 0 {
+			return
 		}
 
-		param := make([]string, 0)
-		for p, _ := range u.Query() {
-			param = append(param, p)
+		for _, param := range reflected {
+			output <- paramCheck{c.url, param}
+		}
+	})
+
+	charChecks := makePool(appendChecks, func(c paramCheck, output chan paramCheck) {
+		wasReflected, err := checkAppend(c.url, c.param, "x55hz33m")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERR %s on PARAM: %s\n", c.url, c.param)
+			return
 		}
 
-		sort.Strings(param)
-
-		key := fmt.Sprintf("%s%s?%s", u.Hostname(), u.EscapedPath(), strings.Join(param, "&"))
-
-		// Only output each host + path + params combination once
-		if _, exists := seen[key]; exists {
-			continue
+		if wasReflected {
+			output <- paramCheck{c.url, c.param}
 		}
-		seen[key] = true
+	})
 
-		// Modify the parameters one by one if the `-s` flag is set
-		if singleMode {
-
-			// Save the original URL
-			originalURL := u.String()
-
-			for i, _ := range param {
-
-				// Restore the original URL
-				u, _ = url.Parse(originalURL)
-
-				qs := url.Values{}
-				for j, p := range param {
-					if i == j {
-						if replaceMode {
-							qs.Set(p, newValue)
-						} else {
-							if (u.Query().Get(p) != newValue) {
-								qs.Set(p, u.Query().Get(p)+newValue)
-							} else {
-								qs.Set(p, newValue)
-							}
-						}
-					} else {
-						qs.Set(p, u.Query().Get(p))
-					}
-				}
-				if (decodeMode){
-					u.RawQuery, _ = url.QueryUnescape(qs.Encode())
-				} else {
-					u.RawQuery = qs.Encode()
-				}
-
-				// Use a buffered writer to write the modified URL to stdout
-				w := bufio.NewWriter(os.Stdout)
-				fmt.Fprintln(w, u)
-				w.Flush()
-			}
-		} else {
-			qs := url.Values{}
-			for param, val := range u.Query() {
-				if replaceMode {
-					qs.Set(param, newValue)
-				} else {
-					qs.Set(param, val[0]+newValue)
-				}
+	done := makePool(charChecks, func(c paramCheck, output chan paramCheck) {
+		output_of_url := []string{c.url, c.param}
+		for _, char := range []string{"\"", "'", "<", ">"} {
+			wasReflected, err := checkAppend(c.url, c.param, "pf1x"+char+"sf1x")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERR on [%s=%s] @ %s\n", c.url, c.param, char)
+				continue
 			}
 
-			if (decodeMode){
-				u.RawQuery, _ = url.QueryUnescape(qs.Encode())
-			} else {
-				u.RawQuery = qs.Encode()
+			if wasReflected {
+				output_of_url = append(output_of_url, char)
+			}
+		}
+		if len(output_of_url) >= 2 {
+			fmt.Printf("%s = %v @ %s\n", output_of_url[1], output_of_url[2:], output_of_url[0])
+		}
+	})
+
+	for sc.Scan() {
+		initialChecks <- paramCheck{url: sc.Text()}
+	}
+
+	close(initialChecks)
+	<-done
+}
+
+func checkReflected(targetURL string) ([]string, error) {
+
+	out := make([]string, 0)
+
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		return out, err
+	}
+
+	// temporary. Needs to be an option
+	req.Header.Add("User-Agent", "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return out, err
+	}
+	if resp.Body == nil {
+		return out, err
+	}
+	defer resp.Body.Close()
+
+	// always read the full body so we can re-use the tcp connection
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return out, err
+	}
+
+	// nope (:
+	if strings.HasPrefix(resp.Status, "3") {
+		return out, nil
+	}
+
+	// also nope
+	ct := resp.Header.Get("Content-Type")
+	if ct != "" && !strings.Contains(ct, "html") {
+		return out, nil
+	}
+
+	body := string(b)
+
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return out, err
+	}
+
+	for key, vv := range u.Query() {
+		for _, v := range vv {
+			if !strings.Contains(body, v) {
+				continue
 			}
 
-			// Use a buffered writer to write the modified URL to stdout
-			w := bufio.NewWriter(os.Stdout)
-			fmt.Fprintln(w, u)
-			w.Flush()
+			out = append(out, key)
 		}
 	}
+
+	return out, nil
+}
+
+func checkAppend(targetURL, param, suffix string) (bool, error) {
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return false, err
+	}
+
+	qs := u.Query()
+	val := qs.Get(param)
+
+	qs.Set(param, val+suffix)
+	u.RawQuery, err = url.QueryUnescape(qs.Encode())
+
+	reflected, err := checkReflected(u.String())
+	if err != nil {
+		return false, err
+	}
+
+	for _, r := range reflected {
+		if r == param {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+type workerFunc func(paramCheck, chan paramCheck)
+
+func makePool(input chan paramCheck, fn workerFunc) chan paramCheck {
+	var wg sync.WaitGroup
+
+	output := make(chan paramCheck)
+	for i := 0; i < 40; i++ {
+		wg.Add(1)
+		go func() {
+			for c := range input {
+				fn(c, output)
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+
+	return output
 }
